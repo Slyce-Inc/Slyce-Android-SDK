8b286e16f479cdcb005480099bdde835b76a06ef
David Svilem <david@onoapps.com>
2015-05-21 10:37:41 +0300

    no message

--- TestApplication/app/src/main/java/com/android/slyce/ProductsGridActivity.java
+++ TestApplication/app/src/main/java/com/android/slyce/ProductsGridActivity.java
@@ -2,12 +2,61 @@ package com.android.slyce;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import com.google.gson.Gson;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
 
 public class ProductsGridActivity extends Activity {
+
+    public static final String PRODUCTS_KEY = "products_key";
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private ArrayList<Product> products;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this).build();
+        ImageLoader.getInstance().init(config);
+
+        products = new ArrayList<Product>();
+        String productsString = getIntent().getStringExtra(PRODUCTS_KEY);
+        try {
+            JSONArray productsJsonArray = new JSONArray(productsString);
+            if (productsJsonArray != null) {
+
+                Gson gson = new Gson();
+                for (int i = 0; i < productsJsonArray.length(); i++) {
+
+                    JSONObject productJson = productsJsonArray.optJSONObject(i);
+                    products.add(gson.fromJson(productJson.toString(), Product.class));
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         setContentView(R.layout.activity_products_grid);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new GridLayoutManager(this, 2);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new ProductsGridAdapter(products);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }

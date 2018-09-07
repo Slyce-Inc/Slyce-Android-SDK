855fe022c14b82afdd7483a6ac1d6f29654772bd
John C. Hunchar <john.hunchar@slyce.it>
2018-09-07 09:48:41 -0400

    [SDK-2329] Public Snippets example should be library (#19)
    * [SDK-2329] Converted app module into Android library module.
    
    * [SDK-2329] Updated snippets to be consistent with iOS.
    
    * [SDK-2329] Minor clean-up.
    
    * [SDK-2329] Minor clean-up.
    
    * [SDK-2329] Minor clean-up.

---
+++ Examples/SlyceSDK-Snippets/snippets/src/main/java/it/slyce/snippets/SlyceSDK_Snippets.java
@@ -0,0 +1,107 @@
+package it.slyce.snippets;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
+
+import org.json.JSONObject;
+
+import it.slyce.sdk.Slyce;
+import it.slyce.sdk.SlyceSearchParameters;
+import it.slyce.sdk.SlyceSearchRequest;
+import it.slyce.sdk.SlyceSession;
+
+public class SlyceSDK_Snippets {
+
+    // Search Parameters
+
+    /**
+     * Demonstrates how to add custom workflow options for a single search task. This assumes that
+     * the Slyce instance has been configured and opened.
+     *
+     * @param context
+     * @throws {@link Exception}
+     */
+    public void addWorkflowOptionsForSingleTask(@NonNull Context context) throws Exception {
+
+        // set up some example data
+        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.sample_img);
+
+        // create a `SlyceSearchParameters` object and set the workflow options using a JSONObject.
+        SlyceSearchParameters searchParams = new SlyceSearchParameters();
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("key1", "value1");
+        jsonObject.put("key2", "value2");
+        searchParams.setWorkflowOptions(jsonObject);
+
+        SlyceSearchRequest request = new SlyceSearchRequest.Builder()
+                .bitmap(bitmap)
+                .searchParameters(searchParams)
+                .build();
+
+        SlyceSession defaultSession = Slyce.getInstance(context).getDefaultSession();
+        if (defaultSession == null) {
+            // handle error
+        } else {
+            defaultSession.startSearchTask(request, "your workflow id", null);
+        }
+    }
+
+    /**
+     * Demonstrates how to add default workflow options to a session. This assumes that the Slyce
+     * instance has been configured and opened.
+     *
+     * @param context
+     * @throws {@link Exception}
+     */
+    public void addDefaultWorkflowOptions(@NonNull Context context) throws Exception {
+
+        // create a `SlyceSearchParameters` object and set the workflow options using a JSONObject.
+        SlyceSearchParameters searchParams = new SlyceSearchParameters();
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("key1", "value1");
+        jsonObject.put("key2", "value2");
+        searchParams.setWorkflowOptions(jsonObject);
+
+        // add to a custom session
+        SlyceSession session = Slyce.getInstance(context).createSession();
+        session.setDefaultSearchParameters(searchParams);
+
+        // or alternatively, add to the default session
+        SlyceSession defaultSession = Slyce.getInstance(context).getDefaultSession();
+        if (defaultSession == null) {
+            // handle error
+        } else {
+            defaultSession.setDefaultSearchParameters(searchParams);
+        }
+    }
+
+    // Analytics
+
+    /**
+     * Demonstrates how to manually trigger analytics, for headless modes. This assumes that the
+     * Slyce instance has been configured and opened.
+     *
+     * @param context
+     * @throws {@link Exception}
+     */
+    public void reportManualAnalytics(Context context) throws Exception {
+
+        // set up some simple example variables
+        String jobId = "abc";
+        String itemId = "123";
+        String itemRevenue = "$9.99";
+        String itemURL = "https://store.com/123";
+        int itemQuantity = 1;
+
+        // track that an image was captured outside of the SlyceSDK
+        Slyce.getInstance(context).getEventTracker().trackCaptureImage();
+
+        // track add to cart
+        Slyce.getInstance(context).getEventTracker().trackAddToCartTap(jobId, itemId, itemRevenue, itemURL, itemQuantity);
+
+        // track successful checkout
+        Slyce.getInstance(context).getEventTracker().trackCheckoutTap(jobId, itemId, itemRevenue, itemURL,itemQuantity);
+    }
+}

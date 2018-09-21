6892e1136ca06a11055241045d76d819ecd78797
John C. Hunchar <john.hunchar@slyce.it>
2018-09-21 09:37:31 -0400

    Added example for exiting to custom list UI.

--- Examples/FullUI/app/src/main/java/it/slyce/slycesdk_fullui_demo_java/CustomCameraActivity.java
+++ Examples/FullUI/app/src/main/java/it/slyce/slycesdk_fullui_demo_java/CustomCameraActivity.java
@@ -1,5 +1,9 @@
 package it.slyce.slycesdk_fullui_demo_java;
 
+import org.json.JSONArray;
+
+import java.util.List;
+
 import it.slyce.sdk.SlyceCustomActivity;
 import it.slyce.sdk.SlyceItemDescriptor;
 
@@ -7,7 +11,25 @@ public class CustomCameraActivity extends SlyceCustomActivity {
 
     @Override
     public boolean shouldDisplayDefaultItemDetailLayerForItem(SlyceItemDescriptor itemDescriptor) {
+
+        // to display your custom detail layer for the selected item,
+        // handle that here and return false.
         ItemDetailActivity.startActivity(this, itemDescriptor.getItem());
+        return false;
+    }
+
+    @Override
+    public boolean shouldDisplayDefaultListLayerForItems(List<SlyceItemDescriptor> itemDescriptors) {
+
+        // to display your custom list layer for the search result items,
+        // handle that here and return false.
+        JSONArray items = new JSONArray();
+
+        for (SlyceItemDescriptor itemDescriptor : itemDescriptors) {
+            items.put(itemDescriptor.getItem());
+        }
+
+        ItemsActivity.startActivity(this, items);
         return false;
     }
 }

4d8d0b3c72032a2d36b2b74773f2513cc4f68d8c
John C. Hunchar <john.hunchar@slyce.it>
2018-09-21 12:49:52 -0400

    Update Full UI Demo to Show Exit to Search Results (#22)
    * Added example for exiting to custom list UI.
    
    * Added custom page labels.

--- Examples/FullUI/app/src/main/java/it/slyce/slycesdk_fullui_demo_java/ItemsActivity.java
+++ Examples/FullUI/app/src/main/java/it/slyce/slycesdk_fullui_demo_java/ItemsActivity.java
@@ -7,24 +7,24 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
-public class ItemDetailActivity extends AppCompatActivity {
+public class ItemsActivity extends AppCompatActivity {
 
-    private static final String KEY_ITEM_JSON = "KEY_ITEM_JSON";
+    private static final String KEY_ITEMS_JSON = "KEY_ITEMS_JSON";
 
     /**
      * Starts the Activity.
      *
      * @param context The context.
-     * @param item    The item to display.
+     * @param items   The items to display.
      */
-    public static void startActivity(Context context, JSONObject item) {
-        Intent intent = new Intent(context, ItemDetailActivity.class);
+    public static void startActivity(Context context, JSONArray items) {
+        Intent intent = new Intent(context, ItemsActivity.class);
 
         try {
-            intent.putExtra(KEY_ITEM_JSON, item.toString(4));
+            intent.putExtra(KEY_ITEMS_JSON, items.toString(4));
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -35,9 +35,9 @@ public class ItemDetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item_detail);
+        setContentView(R.layout.activity_items);
 
-        TextView itemJson = findViewById(R.id.itemDetail_textView);
+        TextView itemJson = findViewById(R.id.items_textView);
         itemJson.setText(getItemJson());
     }
 
@@ -46,7 +46,7 @@ public class ItemDetailActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         if (intent != null) {
-            itemJson = intent.getStringExtra(KEY_ITEM_JSON);
+            itemJson = intent.getStringExtra(KEY_ITEMS_JSON);
         }
 
         return itemJson != null ? itemJson : "";

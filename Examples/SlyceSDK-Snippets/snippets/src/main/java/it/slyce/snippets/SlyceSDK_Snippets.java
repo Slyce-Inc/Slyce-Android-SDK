3d136590aea1aaba1e31c7c1944f728560995d66
Dan Boehmke <dan.boehmke@slyce.it>
2019-02-07 16:11:33 -0500

    Add in Find Similar

--- Examples/SlyceSDK-Snippets/snippets/src/main/java/it/slyce/snippets/SlyceSDK_Snippets.java
+++ Examples/SlyceSDK-Snippets/snippets/src/main/java/it/slyce/snippets/SlyceSDK_Snippets.java
@@ -5,12 +5,18 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import it.slyce.sdk.Slyce;
 import it.slyce.sdk.SlyceSearchParameters;
 import it.slyce.sdk.SlyceSearchRequest;
 import it.slyce.sdk.SlyceSession;
+import it.slyce.sdk.SlyceWorkflowNames;
+import it.slyce.sdk.exception.SlyceInvalidSessionException;
+import it.slyce.sdk.exception.SlyceMissingGDPRComplianceException;
+import it.slyce.sdk.exception.SlyceNotOpenedException;
+import it.slyce.sdk.exception.SlyceSearchTaskBuilderException;
 
 public class SlyceSDK_Snippets {
 
@@ -103,5 +109,58 @@ public class SlyceSDK_Snippets {
 
         // track successful checkout
         Slyce.getInstance(context).getEventTracker().trackCheckoutTap(jobId, itemId, itemRevenue, itemURL,itemQuantity);
+    }
+
+    /**
+     * Demonstrates how to use the find similar feature. This can be used with an item_id or
+     * an url to am image of a product
+     *
+     * @param context
+     */
+    public void onFindSimilarClick(@NonNull Context context) {
+        // A SlyceSession is needed.
+        SlyceSession session = null;
+
+        try {
+            session = Slyce.getInstance(context).createSession();
+        } catch (SlyceMissingGDPRComplianceException e) {
+            e.printStackTrace();
+        } catch (SlyceNotOpenedException e) {
+            e.printStackTrace();
+        }
+
+        // The payload is what is used to compare against the the other products
+        String payload = "http://inventory.store.com/product";
+
+        // Set up a SlyceSearchRequest
+        SlyceSearchRequest request = null;
+
+        // Fill the request based on the type of payload.
+        if (payload.startsWith("http")) {
+            try {
+                request = new SlyceSearchRequest.Builder().imageUrl(payload).build();
+            } catch (SlyceSearchTaskBuilderException e) {
+                e.printStackTrace();
+            }
+        } else {
+            SlyceSearchParameters parameters = new SlyceSearchParameters();
+            JSONObject workflowOptions = new JSONObject();
+            try {
+                workflowOptions.put("item_id", payload);
+                parameters.setWorkflowOptions(workflowOptions);
+                request = new SlyceSearchRequest.Builder().searchParameters(parameters).build();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (SlyceSearchTaskBuilderException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Start the search task and listen for the results with a slyceSearchTaskListener
+        try {
+            session.startSearchTask(request, SlyceWorkflowNames.WORKFLOW_FIND_SIMILAR, null);
+        } catch (SlyceInvalidSessionException e) {
+            e.printStackTrace();
+        }
     }
 }
